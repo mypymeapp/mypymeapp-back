f6ec42f7d4b06e1193b354deeb3c6cf029d0b6db
Pedro M. <pmo.martinez@gmail.com>
2025-09-05 22:03:29 -0400

    Se creo el modulo nuevamente de auth con autenticacion con cookies

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,47 @@
+import { Controller, Post, Body, Res, Get, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { SignupDto, SignupResponseDto } from 'src/auth/dto/signup.dto';
+import { ApiBody, ApiCreatedResponse, ApiBearerAuth } from '@nestjs/swagger';
+import { SigninDto, SigninResponseDto } from 'src/auth/dto/signin.dto';
+import type { Response } from 'express';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { GetCurrentUser, type CurrentUser } from './decorators/current-user.decorator';
+import { ApiTags } from '@nestjs/swagger';
+
+@Controller('auth')
+@ApiTags('Autenticación')
+export class AuthController {
+
+    constructor(private authService: AuthService) {}
+
+    @ApiBody({ type: SignupDto })
+    @ApiCreatedResponse({ type: SignupResponseDto })
+
+    @Post('register')
+    async register(@Body() dto: SignupDto, @Res({ passthrough: true }) res: Response) {
+        return this.authService.signUp(dto, res);
+    }
+
+    @ApiBody({ type: SigninDto })
+    @ApiCreatedResponse({ type: SigninResponseDto })
+
+    @Post('login')
+    async login(@Body() dto: SigninDto, @Res({ passthrough: true }) res: Response) {
+        return this.authService.signIn(dto, res);
+    }
+
+    @Post('logout')
+    async logout(@Res({ passthrough: true }) res: Response) {
+        return this.authService.signOut(res);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @ApiBearerAuth()
+    @Get('profile')
+    async getProfile(@GetCurrentUser() user: CurrentUser) {
+        return {
+            message: 'Perfil obtenido exitosamente',
+            user
+        };
+    }
+}
---
+++ src/auth/dto/signup.dto.ts
@@ -0,0 +1,43 @@
+import { IsDate, IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class SignupDto {
+
+    @ApiProperty({ example: "John Doe" })
+    @IsString() 
+    name: string;
+
+    @ApiProperty({ example: "user@example.com" })
+    @IsEmail() 
+    email: string;
+
+    @ApiProperty({ example: "password" })
+    @MinLength(8) 
+    passwordHash: string;
+
+    @ApiProperty({ example: "https://example.com/avatar.jpg" })
+    @IsOptional() 
+    @IsString() 
+    avatarUrl?: string;
+}
+
+export class SignupResponseDto {
+
+    @ApiProperty({ example: "123e4567-e89b-12d3-a456-426614174000" })
+    @IsString()
+    id: string;
+    @ApiProperty({ example: "John Doe" })
+    @IsString()
+    name: string;
+    @ApiProperty({ example: "user@example.com" })
+    @IsEmail()
+    email: string;
+    @ApiProperty({ example: "https://example.com/avatar.jpg" })
+    @IsOptional()
+    @IsString()
+    avatarUrl?: string;
+    @ApiProperty({ example: "2023-01-01T00:00:00.000Z" })
+    @IsDate()
+    createdAt: Date;
+    
+}
