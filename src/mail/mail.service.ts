06fc2ee1fbcad99a406955d3128775a90e665474
Guillermo_Rivero <gilerme1980@gmail.com>
2025-09-11 17:41:06 -0300

    add mailModule

---
+++ src/mail/mail.service.ts
@@ -0,0 +1,35 @@
+import * as nodemailer from 'nodemailer';
+import { ConfigService } from '@nestjs/config';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class EmailService {
+    private transporter;
+
+    constructor(private configService: ConfigService) {
+        this.transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true, // Usar SSL para el puerto 465
+        auth: {
+            user: this.configService.get<string>('GMAIL_USER'),
+            pass: this.configService.get<string>('GMAIL_APP_PASSWORD'),
+        },
+        });
+    }
+
+    async sendEmail(to: string, subject: string, html: string) {
+        const info = await this.transporter.sendMail({
+            from: `"Tu Aplicación" <${this.configService.get<string>('GMAIL_USER')}>`,
+            to,
+            subject,
+            html,
+        });
+
+        return {
+            accepted: info.accepted,
+            rejected: info.rejected,
+            messageId: info.messageId,
+        };
+    }
+}
