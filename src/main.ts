76de1c888348ceab118e986d6c16edf4d1d44ec5
APaggiHitta <alvaropaggi@gmail.com>
2025-09-14 20:48:06 -0300

    cors correction

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://mypymeapp-front.vercel.app/'],
+    origin: ['http://localhost:3000', 'https://mypymeapp-front.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
